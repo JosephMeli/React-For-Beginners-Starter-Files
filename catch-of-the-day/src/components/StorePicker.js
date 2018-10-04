99ff87b51a2138d50cd099618ba3c2b0306a208d
Joseph Meli <joemeli631@gmail.com>
2018-10-04 16:41:07 -0400

    made it through video six

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,17 @@
-import React from 'react';
-
+import React from "react";
 
 class StorePicker extends React.Component {
-    render(){
-        return(
-            <p>Hello World!;</p>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <form className="store-selector">
+          <h2>Please Enter a Store Name</h2>
+          <input type="text" placeholder="Store Name" />
+          <button type="submit">Visit Store</button>
+        </form>
+      </React.Fragment>
+    );
+  }
 }
 
 export default StorePicker;
